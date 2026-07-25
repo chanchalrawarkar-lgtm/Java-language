48deb14395ba6b17e6eb7c05fc0bb58302985560
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-26 00:01:08 +0530

    Add hello class to print 'hi' four times
    Added a new class to print 'hi' repeatedly.

--- hello.java
+++ hello.java
@@ -51,3 +51,15 @@ import java.util.Scanner;
          }
      }
   }
+
+// to print hi repeatedly
+class hello {
+     public static void main(String[] args) {
+         int i =1;
+     while(i<=4)
+     {
+         System.out.println("hi ");
+         i++;
+     }    
+ }
+ }
