4e7d4ba6039daaeadaef375622afd22900b0336f
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-27 23:30:35 +0530

    Add main method for array output

--- hello.java
+++ hello.java
@@ -124,3 +124,12 @@ class hello {
             }
         }
  }
+
+class hello {
+        public static void main (String args[]) {
+            int[] num= {10,20,40,70};
+            for(int i=0;i<num.length;i++){
+                System.out.println(num[i]);
+            }
+        }
+ }
