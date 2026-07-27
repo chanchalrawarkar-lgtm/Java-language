d49e74a47027de910f0be6ade69cdf2caf044439
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-27 23:25:18 +0530

    Add main method for printing numbers

--- hello.java
+++ hello.java
@@ -116,3 +116,11 @@ class hello {
         }
     }
         
+ class hello {
+        public static void main (String args[]) {
+            int[] num= {10,20,30,40,50};
+            for(int i=0;i<num.length;i++){
+                System.out.println(num[i]);
+            }
+        }
+ }
