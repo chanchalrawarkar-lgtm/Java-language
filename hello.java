570c959712fe8dc2c3fa55f5415aecc19eb9617e
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-27 23:14:35 +0530

    Add sum calculation by array
    Added a new class 'hello' with a main method that calculates the sum of an integer array.

--- hello.java
+++ hello.java
@@ -79,3 +79,15 @@ class hello {
         System.out.println(num[2]);
     }
 }
+
+// arrayex
+class hello {
+        public static void main (String args[]) {
+            int[] num= {10,20,30,40,50};
+            int sum=0;
+            for(int i=0; i<num.length; i++) {
+                sum=sum+num[i];
+            }
+                System.out.println(sum);
+            }
+        }
