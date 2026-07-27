62ab0231cb3abc0dd17dc6ae1e910fd1cd296d4b
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-27 23:02:33 +0530

    Add hello and array example
    Added a new class 'hello' with a main method that prints 'Hello, World!' and elements from an array.

--- hello.java
+++ hello.java
@@ -63,3 +63,19 @@ class hello {
      }    
  }
  }
+
+// hello world
+class hello {
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+}
+}
+
+// array
+class hello {
+    public static void main (String args[]) {
+        int[] num= {10,20,30,40,50};
+        System.out.println(num[0]);
+        System.out.println(num[2]);
+    }
+}
