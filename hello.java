7331e959e7b3969159a5feba9843cc9fca80776e
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-31 21:15:11 +0530

    basic array

--- hello.java
+++ hello.java
@@ -575,4 +575,17 @@ class hello {
 }
 }
 
-
+import java.util.Scanner;
+class hello {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int num[] = new int[size];
+         for(int i=0; i<size; i++) {
+            num[i]=sc.nextInt();
+         }
+        for(int i=0; i<num.length;i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
