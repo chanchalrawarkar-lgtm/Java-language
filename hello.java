b7f95284621376025ecb4ac007408256a1c09902
chanchalrawarkar-lgtm <chanchalrawarkar@gmail.com>
2026-07-31 21:50:30 +0530

    Implement search functionality in hello.java
    Added functionality to input an array and find the index of a specified number.

--- hello.java
+++ hello.java
@@ -607,3 +607,23 @@ class hello {
         }
     }
 }
+
+// to take x and find its place
+import java.util.Scanner;
+class hello {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int num[] = new int[size];
+        for (int i = 0; i <size; i++) {
+            num[i]=sc.nextInt();
+            
+        }
+        int x=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            if(x==num[i]) {
+            System.out.println(i);
+            }
+        }
+    }
+    }
